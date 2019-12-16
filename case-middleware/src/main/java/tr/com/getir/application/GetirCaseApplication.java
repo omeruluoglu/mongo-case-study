842c30177004f8444aedd386a99d49b5e0ff7325
omeruluoglu <omeruluoglu@gmail.com>
2019-12-16 08:38:12 +0300

    Changed controller structure and initialized servlet application

--- case-middleware/src/main/java/tr/com/getir/application/GetirCaseApplication.java
+++ case-middleware/src/main/java/tr/com/getir/application/GetirCaseApplication.java
@@ -3,6 +3,8 @@ package tr.com.getir.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -12,11 +14,16 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
  */
 @SpringBootApplication (exclude = { DataSourceAutoConfiguration.class })
 @ComponentScan(basePackages = {"tr.com.getir"})
-@PropertySource("file:/usr/local/tomcat/properties/application.properties")
+@PropertySource("classpath:application.properties")
 @EnableMongoRepositories(basePackages = "tr.com.getir.persistency.dao")
-public class GetirCaseApplication {
+public class GetirCaseApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(GetirCaseApplication.class, args);
+    }
+
+    @Override
+    public SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(GetirCaseApplication.class);
     }
 }
